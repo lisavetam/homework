836cf170718bd7c3e2a94cd23334ed2feb71a9de
elizaveta schlegel <elizaveta.schlegel@gmail.com>
2024-01-23 14:13:36 +0100

    modificated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,99 @@
 
 import './App.css';
+import { useState } from 'react';
+import { data } from './data';
+
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+
+const [books, setBooks] =useState (data);
+const removeBooks = (id) => {
+let newBooks = books.filter(book => book.id !==id);
+setBooks (newBooks)
+}
+const [book, setBook] =useState (0);
+const {id, name,  image} = data[book];
+const previousBook =() => {
+  setBook (book =>{
+book --;
+if (book <0) {
+book =data.length -1;
+}
+return book;
+  })
+
+
+  }
+   
+  const nextBook =() =>{
+    setBook (book =>{
+      book ++;
+      if (book > data.length -1){
+    book = 0
+      }
+      return book;
+    })
+    }
+    
+
+return(
+  <div>
+  <div className='container'>
+<h1>  {books.length} лучших книг</h1>
+  </div>
+  <div className='container'>
+  <h2>Американский журнал «Newsweek» опубликовал рейтинг «100 лучших книг всех времен и народов». Ниже представлена первая 5ка из него:</h2>
+  </div>
+  {books.map((element => {
+    const {id,name, image} = element;
+
+    return(
+      <div key={id}>
+        <div className='container'>
+<h2>{id}- {name}</h2>
+        </div>
+        <div className='container'>
+<img src={image} width="300px"/>
+        </div>
+        <div className='container'>
+<button onClick={() => removeBooks(id)}>Remove</button>
+        </div>
+      </div>
+    )
+  }))}
+  <div className='container'> 
+  <button onClick={() => setBooks([])}>delete all</button>
+  </div>
+
+   <div>
+ 
+          <div>
+      
+      <div className='container'>
+           <img src={image} width="400px" alt='book'/>
+           </div>
+            <div className='container'>
+           <h1>{id}-{name}</h1>
+           </div>
+
+         
+      <div className='btn container'>
+      <button onClick={previousBook}>Previous</button>
+      <button onClick={nextBook}>Next</button>
+      </div>
+          </div>
+        
+      
+    </div> 
+
+  </div>
+        )
+        
 }
 
 export default App;
+
+
+
+
+
---
+++ src/data.js
@@ -0,0 +1,28 @@
+export const data = [
+ {
+ id: 1,
+ name: "Лев Толстой «Война и мир»",
+ image: "https://cdn.eksmo.ru/v2/ITD000000001067433/COVER/cover1__w820.jpg"
+},
+ {
+ id: 2,
+ name: "Джордж Оруэлл «1984»",
+ image: "https://cdn.eksmo.ru/v2/ITD000000001119955/COVER/cover1__w820.jpg"
+},
+ {
+ id: 3,
+ 
+ name: "Джеймс Джойс «Улисс»",
+ image: "https://cdn.eksmo.ru/v2/ITD000000000829297/COVER/cover1__w820.jpg"
+},
+ {
+ id: 4,
+ name: "Владимир Набоков «Лолита»",
+ image: "https://cdn.azbooka.ru/cv/w1100/42872371-9a59-4270-b37c-c355c75dc115.jpg"
+},
+ {
+ id: 5,
+ name: "Уильям Фолкнер «Звук и ярость»",
+ image:"https://img4.labirint.ru/rc/a2701e4c51ab9d998b71fd5bc3bb4f10/363x561q80/books80/791644/cover.jpg?1614237923"
+},
+]
